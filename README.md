7f62e77d8164933b999c53a5da93666a25966a9d
Robert Ridley <robert.n.ridley@gmail.com>
2019-03-04 02:41:09 +0800

    web api for grammar correction

--- README.md
+++ README.md
@@ -1,18 +1,81 @@
-python-ginger
-=============
-Simple English grammar checker with ***unofficial*** [Ginger](http://www.getginger.jp/) API.
-This grammar checker will fix grammar mistakes.
+# 语法纠错
 
-How to use
------
+### 文档说明
+
+#### 运行环境
+* python 3.5.1
+
+#### 设置环境
+* 输入 `cd writing_error_correction`
+* 生成一个虚拟环境: `python3 -m venv venv`
+* 启动虚拟环境：`source venv/bin/activate`
+* 下载dependencies：`pip install -r requirements.txt`
+
+#### 运行说明
+* `cd web`
+* `export FLASK_APP=app.py`
+* `flask run`
+* 目前 端口设置 为3000
+
+#### URL
+* http://127.0.0.1:5000/api/v1/textCheck
+
+#### HTTP请求方式
+* POST
+
+#### 提交参数
+| 参数名		| 必填  	| 类型		| 说明 				  	        |
+| ----------|:-----:| ---------:|------------------------------:|
+| text   	| Y    	| string 	| 需要纠错的文本，必填，字符串类型  	|
+
+#### 返回参数
+| 参数名		            | 说明  	               | 举例 				                        |
+| ----------------------|:--------------------:|-------------------------------------------:|
+| data   	            | 错误信息              | 该值为json格式数组                           |
+| word   	            | 出错词汇              | neve 	                                    |
+| type   	            | 警告类型              | spelling /grammar                          |
+| substitute   	        | 替换词                | have            	                        |
+| description           | 警告说明              | Consider changing the verb form            |
+| existence             | 词汇是否存在           | true (布尔值)                               |
+| explanation           | 错误解释    	       | Modal verbs are auxiliary verbs used...    |
+| example             	| 警告正确示例           | 该值为json格式三维数组       	                |
+| example.0.incorrect   | 示例第一条的错误举例    | Petar can sings in...                     	|
+| example.0.correct     | 示例第一条的正确举例    | 该值为一维数组                           	|
+
+#### 返回例子
+请求：
 ```
-# python 3.x
-$ python ginger.py <Some English Text>
-
-# python 2.x
-$ python ginger_python2.py <Some English Text>
+{
+	"text": "She likes he."
+}
 ```
 
-Reference
------
-[Ginger API を試してみた - にひりずむ::しんぷる (http://blog.livedoor.jp/xaicron/archives/54466736.html)](http://blog.livedoor.jp/xaicron/archives/54466736.html)
+返回：
+```
+{
+    "errno": 0,
+    "data": [
+        {
+            "type": "grammar",
+            "substitute": "him",
+            "explanation": "that male; objective male pronoun",
+            "example": [
+                {
+                    "correct": [
+                        "She likes him."
+                    ],
+                    "incorrect": "She likes he."
+                }
+            ],
+            "description": "objective male pronoun, used to refer to a male human or animal",
+            "word": "he",
+            "existence": true
+        }
+    ],
+    "message": "success"
+}
+```
+
+
+
+
---
+++ web/app.py
@@ -0,0 +1,120 @@
+import json
+from flask import Flask, request, Response
+import urllib.parse
+import urllib.request
+from urllib.error import HTTPError
+from urllib.error import URLError
+
+app = Flask(__name__)
+
+
+def get_ginger_url(text):
+    API_KEY = "6ae0c3a0-afdc-4532-a810-82ded0054236"
+    scheme = "http"
+    netloc = "services.gingersoftware.com"
+    path = "/Ginger/correct/json/GingerTheText"
+    params = ""
+    query = urllib.parse.urlencode([
+        ("lang", "US"),
+        ("clientVersion", "2.0"),
+        ("apiKey", API_KEY),
+        ("text", text)])
+    fragment = ""
+    return(urllib.parse.urlunparse((scheme, netloc, path, params, query, fragment)))
+
+
+def get_ginger_result(text):
+    url = get_ginger_url(text)
+    try:
+        response = urllib.request.urlopen(url)
+    except HTTPError as e:
+            print("HTTP Error:", e.code)
+            return e.code, True
+    except URLError as e:
+            print("URL Error:", e.reason)
+            return e.reason, True
+    try:
+        result = json.loads(response.read().decode('utf-8'))
+    except ValueError:
+        print("Value Error: Invalid server response.")
+        return "Value Error: Invalid server response", True
+
+    return result, False
+
+
+def convert_to_new_json_format(data, original_text):
+    json_data = {
+        "errno": 0,
+        "message": "success",
+        "data": []
+    }
+
+    responses = data["LightGingerTheTextResult"]
+    if len(responses) == 0:
+        return json_data
+    fix_val = 0
+    fixed_text = original_text
+    for item in responses:
+        if "ShouldReplace" in item:
+            if item["ShouldReplace"] != "True":
+                item_data = {}
+                item_data["word"] = original_text[item["From"]:(item["To"]+1)]
+                item_data["substitute"] = item["Suggestions"][0]["Text"]
+                if item["Mistakes"][0]["CanAddToDict"]:
+                    item_data["type"] = "spelling"
+                else:
+                    item_data["type"] = "grammar"
+                if "Definition" in item["Mistakes"][0]:
+                    item_data["description"] = item["Mistakes"][0]["Definition"]
+                else:
+                    item_data["description"] = ""
+                item_data["existence"] = True
+                if "Definition" in item["Suggestions"][0]:
+                    item_data["explanation"] = item["Suggestions"][0]["Definition"]
+                else:
+                    item_data["explanation"] = ""
+                fixed_text = fixed_text[:item["From"]-fix_val] + item_data["substitute"] + fixed_text[(item["To"]+1)-fix_val:]
+                item_data["example"] = [
+                    {
+                        "incorrect": original_text,
+                        "correct": []
+                    }
+                ]
+                fix_val += (item["To"]+1) - item["From"] - len(item_data["substitute"])
+
+                json_data["data"].append(item_data)
+    for row in json_data["data"]:
+        row["example"][0]["correct"].append(fixed_text)
+    return json_data
+
+
+@app.route("/api/v1/textCheck", methods = ['POST'])
+def check_grammar():
+    req_json = request.get_json()
+    original_text = req_json['text']
+    if len(original_text) > 600:
+        print("You can't check more than 600 characters at a time.")
+        return Response(json.dumps({"error": "You can't check more than 600 characters at a time."}),
+                        status=200, mimetype='application/json')
+    results, error = get_ginger_result(original_text)
+    if error:
+        print("Error")
+        return Response(json.dumps({"error": error}), status=200, mimetype='application/json')
+    new_result = convert_to_new_json_format(results, original_text)
+    res = json.dumps(new_result)
+    res = Response(res, status=200, mimetype='application/json')
+    return res
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return Response(json.dumps({'error': 'Not found'}), status=404, mimetype='application/json')
+
+
+if __name__ == '__main__':
+    app.run(
+        host='0.0.0.0',
+        port=3000,
+    )
+
+
